df9c0531bd7f85b3d807f2e381798c6cef297354
Harsh <harshjiladiya99@gmail.com>
2022-01-21 13:22:57 +0530

    test

---
+++ src/demo.java
@@ -0,0 +1,12 @@
+
+public class demo {
+	
+	public static void main (String[] args)
+	{
+		Syso();
+	}
+ private static void Syso() {
+	// TODO Auto-generated method stub
+	 System.out.println(" this is the demo project that is just for the demo purpose .");
+}
+}
